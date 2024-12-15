99803d8f995785ff5d22187344e9ac78bee8c4a6
vishwajeetsuhaspatil <bhagyashreesushv2@gmail.com>
2024-12-15 20:45:56 +0530

    np

---
+++ a.py
@@ -0,0 +1,6 @@
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+a= a+b
+b= a-b
+a= a-b
+print(a,b)
---
+++ np.py
@@ -0,0 +1,6 @@
+def abc(a,b):
+    a= a^b
+    b= a^b
+    a= a^b
+    print(a,b)
+abc(10,2)    
